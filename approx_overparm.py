31c30fac196968553c04fb0f1381b3d0c81e1612
Brando Miranda <brando90@mit.edu>
2017-07-06 01:56:48 -0400

    sgd runs

--- approx_overparm.py
+++ approx_overparm.py
@@ -4,14 +4,18 @@ import matplotlib.pyplot as plt
 
 def poly_kernel_matrix( x,D ):
     N = len(x)
-    for n in range(N)
-        for d in range(D+1)
-            Kern[n,d+1] = x[n]**d;
+    Kern = np.zeros( (N,D+1) )
+    for n in range(N):
+        for d in range(D+1):
+            Kern[n,d] = x[n]**d;
     return Kern
 
 def get_batch(X,Y,M):
-    batch_xs = np.random.choice(X,size=M)
-    batch_ys = np.random.choice(Y,size=M)
+    N = len(Y)
+    valid_indices = np.array( range(N) )
+    batch_indices = np.random.choice(valid_indices,size=M,replace=False)
+    batch_xs = X[batch_indices,:]
+    batch_ys = Y[batch_indices]
     return batch_xs, batch_ys
 
 def main():
@@ -25,32 +29,36 @@ def main():
     B=1000
     nb_iter = 400000
     ##
-    x = linspace(lb,ub,5)
+    x = np.linspace(lb,ub,5)
     y = np.array([0,1,0,-1,0]).transpose()
-    X_true = poly_kernel_matrix( x,D )
-    c_true = linalg.pinv(X_true)*y
+    X_true = poly_kernel_matrix( x,Degree_true )
+    c_true = np.linalg.pinv(X_true)*y
     ##
     # N = 5000
     ##
     #l2loss = tf.nn.l2_loss(y - )
     X = tf.placeholder(tf.float32, [None, D_true])
-    Y = tf.placeholder(tf.float32, [None, 1])
+    Y = tf.placeholder(tf.float32, [None])
     w = tf.Variable( tf.zeros([D_sgd,1]) )
-    y_ = X*w
-    l2loss = tf.reduce_sum(Y - W*c)
+    f = X*w
+    l2loss = tf.reduce_sum(Y - f)
+    #
+    M = 5
     train_step = tf.train.GradientDescentOptimizer(0.5).minimize(l2loss)
     with tf.Session() as sess:
         tf.global_variables_initializer().run()
         # Train
         for _ in range(1000):
-            batch_xs, batch_ys = get_batch(X,Y,M)
-            sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+            batch_xs, batch_ys = get_batch(X_true,y,M)
+            sess.run(train_step, feed_dict={X: batch_xs, Y: batch_ys})
         #
         c_sgd = w.eval()
         print('c_sgd: ',c_sgd)
-        print('c_true: 'c_true)
+        print('c_true: ',c_true)
         print(' c_sgd - c_true', np.norm(c_sgd - c_true,2))
 
 
-if __name__ == '__name__':
+if __name__ == '__main__':
+    print('__main__')
     main()
+    print('end')
