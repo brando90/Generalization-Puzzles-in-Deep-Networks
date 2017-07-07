585e3eee01774dfc301f079538c29c18e1d8af1c
Brando Miranda <brando90@mit.edu>
2017-07-07 14:23:09 -0400

    added RLS

--- approx_overparm.py
+++ approx_overparm.py
@@ -38,6 +38,12 @@ def poly_kernel_matrix( x,D ):
             Kern[n,d] = x[n]**d;
     return Kern
 
+def get_RLS_soln( X,Y,lambda_rls):
+    N,D = X.shape
+    XX_lI = np.dot(X.transpose(),X) + lambda_rls*N*np.identity(D)
+    w = np.dot( np.dot( np.linalg.inv(XX_lI), X.transpose() ), Y)
+    return w
+
 def get_batch(X,Y,M):
     N = len(Y)
     valid_indices = np.array( range(N) )
@@ -53,11 +59,13 @@ def main(argv=None):
     ##
     lb, ub = FLAGS.lb, FLAGS.ub
     N = FLAGS.N
-    Degree = 100
+    Degree = 19
     D_sgd = Degree+1
-    D_ = Degree+1
+    D_pinv = Degree+1
+    D_rls = D_pinv
     B=1000
     nb_iter = 100000
+    lambda_rls = 0.1
     #report_error_freq = nb_iter/4
     ##
     np.set_printoptions(suppress=True)
@@ -66,8 +74,9 @@ def main(argv=None):
     y.shape = (N,1)
     X_true = poly_kernel_matrix( x,Degree ) # [N, D] = [N, Degree+1]
     c_pinv = np.dot(np.linalg.pinv(X_true),y) # [N,1]
-    if D_mdl == N:
-        D_true = D_mdl
+    c_rls = get_RLS_soln(X_true,y,lambda_rls)
+    if D_pinv == N:
+        D_true = D_pinv
         c_true = c_pinv
     print(X_true)
     print('\ny: ',y)
@@ -78,7 +87,7 @@ def main(argv=None):
     ##
     graph = tf.Graph()
     with graph.as_default():
-        X = tf.placeholder(tf.float32, [None, D_mdl])
+        X = tf.placeholder(tf.float32, [None, D_pinv])
         Y = tf.placeholder(tf.float32, [None,1])
         #w = tf.Variable( tf.zeros([D_sgd,1]) )
         w = tf.Variable( tf.truncated_normal([D_sgd,1],mean=0.0,stddev=1.0) )
@@ -96,9 +105,10 @@ def main(argv=None):
         g_2 = tf.norm(var_grad)
         tf.summary.scalar('norm(g)', g_2)
         #
-        M = 5
+        M = 2
+        eta = 0.02
         global_step = tf.Variable(0, trainable=False)
-        learning_rate = tf.train.exponential_decay(learning_rate=0.02, global_step=global_step,
+        learning_rate = tf.train.exponential_decay(learning_rate=eta, global_step=global_step,
             decay_steps=nb_iter/2, decay_rate=1, staircase=True)
         train_step = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(loss)
     #
@@ -132,29 +142,35 @@ def main(argv=None):
         c_pinv.shape = (len(c_pinv),1)
         #
         print()
-        print('degree: ',Degree)
-        print('nb_iter: ',nb_iter)
         print('c_sgd: ')
         print(c_sgd)
         print('c_pinv: ')
         print(c_pinv)
+        print('\n||c_sgd - c_pinv|| = ', np.linalg.norm(c_sgd - c_pinv,2))
         #
         print('norm(c_sgd): ', np.linalg.norm(c_sgd))
         print('norm(c_pinv): ', np.linalg.norm(c_pinv))
+        print('norm(c_rls): ', np.linalg.norm(c_rls))
         #
-        print(' ||c_sgd - c_pinv|| = ', np.linalg.norm(c_sgd - c_pinv,2))
+        print('Degree={}\n nb_iter={}\n batch-size= {} \n learning_rate= {}\n'.format(Degree,nb_iter,M,eta) )
+        #
         print(' J(c_sgd) = ', sess.run(fetches=loss, feed_dict={X: X_true, Y: y}) )
         Xc_pinv = np.dot(X_true,c_pinv)
         print( ' J(c_pinv) = ',(1/N)*(np.linalg.norm(y-Xc_pinv)**2) )
+        Xc_rls = np.dot(X_true,c_rls)
+        print( ' J(c_rls) = ',(1/N)*(np.linalg.norm(y-Xc_rls)**2) )
         #
         print('\a')
-        #
+        ##
         x_horizontal = np.linspace(lb,ub,1000)
         X_plot = poly_kernel_matrix(x_horizontal,D_sgd-1)
+        #plots
         p_sgd, = plt.plot(x_horizontal, np.dot(X_plot,c_sgd))
-        p_true, = plt.plot(x_horizontal, np.dot(X_plot,c_pinv))
+        p_pinv, = plt.plot(x_horizontal, np.dot(X_plot,c_pinv))
+        p_rls, = plt.plot(x_horizontal, np.dot(X_plot,c_rls))
         p_data, = plt.plot(x,y,'ro')
-        plt.legend([p_sgd,p_true,p_data],['sgd curve ','true min norm','data points'])
+        #
+        plt.legend([p_sgd,p_pinv,p_rls,p_data],['sgd curve degree='+str(D_sgd-1),'min norm (pinv) degree='+str(D_pinv-1),'rls regularization lambda={} degree={}'.format(lambda_rls,D_rls-1),'data points'])
         plt.ylabel('f(x)')
         plt.show()
 
