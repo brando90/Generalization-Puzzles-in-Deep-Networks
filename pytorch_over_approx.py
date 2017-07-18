c5972a9aa3347636db2fc3ec17a6d94630abda70
Brando Miranda <brando90@mit.edu>
2017-07-18 19:10:28 -0400

    mutlple layer quadratic activation

--- pytorch_over_approx.py
+++ pytorch_over_approx.py
@@ -54,13 +54,20 @@ def get_batch(X,Y,M):
 
     return batch_xs, batch_ys
 
+def index_batch(X,batch_indices,dtype):
+    if len(X.shape) == 1: # i.e. dimension (M,) just a vector
+        batch_xs = torch.FloatTensor(X[batch_indices]).type(dtype)
+    else:
+        batch_xs = torch.FloatTensor(X[batch_indices,:]).type(dtype)
+    return batch_xs
+
 def get_batch2(X,Y,M,dtype):
     X,Y = X.data.numpy(), Y.data.numpy()
     N = len(Y)
     valid_indices = np.array( range(N) )
     batch_indices = np.random.choice(valid_indices,size=M,replace=False)
-    batch_xs = torch.FloatTensor(X[batch_indices,:]).type(dtype)
-    batch_ys = torch.FloatTensor(Y[batch_indices]).type(dtype)
+    batch_xs = index_batch(X,batch_indices,dtype)
+    batch_ys = index_batch(Y,batch_indices,dtype)
     return Variable(batch_xs, requires_grad=False), Variable(batch_ys, requires_grad=False)
 
 def get_old():
@@ -75,20 +82,20 @@ def main(argv=None):
     start_time = time.time()
     ##
     np.set_printoptions(suppress=True)
-    lb, ub = 0, 1
+    lb, ub = -1, 1
     ## true facts of the data set
     N = 10
     Degree_true = 4
     D_true = Degree_true+1
     ## mdl degree and D
-    Degree_mdl = 100000
+    Degree_mdl = 4
     D_sgd = Degree_mdl+1
     D_pinv = Degree_mdl+1
     D_rls = D_pinv
     ## sgd
     M = 5
-    eta = 0.01 # eta = 1e-6
-    nb_iter = int(3.75*100)
+    eta = 0.1 # eta = 1e-6
+    nb_iter = int(100000)
     lambda_rls = 0.0001
     ##
     x_true = np.linspace(lb,ub,N) # the real data points
@@ -102,26 +109,51 @@ def main(argv=None):
     c_pinv = np.dot(np.linalg.pinv(X_mdl),y) # [D_pinv,1]
     c_rls = get_RLS_soln(X_mdl,y,lambda_rls) # [D_pinv,1]
     ## TORCH
+    X_mdl = x_true
     dtype = torch.FloatTensor
     # dtype = torch.cuda.FloatTensor # Uncomment this to run on GPU
     X_mdl = Variable(torch.FloatTensor(X_mdl).type(dtype), requires_grad=False)
+    X_mdl = X_mdl.view(N,1)
     y = Variable(torch.FloatTensor(y).type(dtype), requires_grad=False)
     ## SGD mdl
-    #w_init = torch.randn(D_sgd,1).type(dtype)
-    w_init = torch.zeros(D_sgd,1).type(dtype)
-    W = Variable(w_init, requires_grad=True)
+    #w_init = 1000*torch.randn(D_sgd,1).type(dtype)
+    # w_init = torch.zeros(D_sgd,1).type(dtype)
+    # W = Variable(w_init, requires_grad=True)
+    D,H_l1 = 1,2
+    #w_init = torch.zeros(D,H_l1).type(dtype)
+    w_init = torch.randn(D,H_l1).type(dtype)
+    W_l1 = Variable(w_init, requires_grad=True)
+
+    H_l2 = 2
+    #w_init = torch.zeros(H_l1,H_l2).type(dtype)
+    w_init = torch.randn(H_l1,H_l2).type(dtype)
+    W_l2 = Variable(w_init, requires_grad=True)
+
+    w_init = torch.zeros(H_l2,1).type(dtype)
+    W_out = Variable(w_init, requires_grad=True)
     ## debug print statements
     print('>>norm(y): ', ((1/N)*torch.norm(y)**2).data.numpy()[0] )
-    print('>>l2_np: ', (1/N)*np.linalg.norm( y.data.numpy()-(np.dot(X_mdl.data.numpy(),W.data.numpy())) )**2 )
-    print('>>l2_loss_torch: ', (1/N)*(X_mdl.mm(W) - y).pow(2).sum().data.numpy()[0] )
-    print('>>(1/N)*(y_pred - y).pow(2).sum(): ', ((1/N)*(X_mdl.mm(W) - y).pow(2).sum()).data[0] )
+    #print('>>l2_np: ', (1/N)*np.linalg.norm( y.data.numpy()-(np.dot(X_mdl.data.numpy(),W.data.numpy())) )**2 )
+    #print('>>l2_loss_torch: ', (1/N)*(X_mdl.mm(W) - y).pow(2).sum().data.numpy()[0] )
+    #print('>>(1/N)*(y_pred - y).pow(2).sum(): ', ((1/N)*(X_mdl.mm(W) - y).pow(2).sum()).data[0] )
     #pdb.set_trace()
     loss_list = []
     grad_list = []
+    Ws = [W_l1,W_l2,W_out]
+    #Ws = [W]
+    #W_avg = Variable(torch.FloatTensor(W.data).type(dtype), requires_grad=False)
+    W_avgs = [Variable(torch.FloatTensor(W_l1.data).type(dtype), requires_grad=False),Variable(torch.FloatTensor(W_l2.data).type(dtype), requires_grad=False),Variable(torch.FloatTensor(W_out.data).type(dtype), requires_grad=False)]
+    # for in range(3):
+    #     w_avg = Variable(torch.FloatTensor(W.data).type(dtype), requires_grad=False)
+    #     W_avgs.append()
     for i in range(nb_iter):
         # Forward pass: compute predicted y using operations on Variables
-        batch_xs, batch_ys = get_batch2(X_mdl,y,M,dtype)
-        y_pred = batch_xs.mm(W)
+        batch_xs, batch_ys = get_batch2(X_mdl,y,M,dtype) # [M, D], [M, 1]
+        #pdb.set_trace()
+        #y_pred = batch_xs.mm(W)
+        a_l1 = batch_xs.mm(W_l1)**2 # [M,H^(1)] = [M,D]x[D,H^(1)]
+        a_l2 = a_l1.mm(W_l2)**2 # [M,H^(2)] = [M,H^(1)]x[H^(1),H^(2)]
+        y_pred = a_l2.mm(W_out) # [M,1] = [M,H^(2)]x[M^(2),1]
         # Compute and print loss using operations on Variables. Now loss is a Variable of shape (1,) and loss.data is a Tensor of shape (1,); loss.data[0] is a scalar value holding the loss.
         #loss = (1/N)*(y_pred - y).pow(2).sum()
         loss = (1/N)*(y_pred - batch_ys).pow(2).sum()
@@ -129,7 +161,8 @@ def main(argv=None):
         loss.backward()
         # Update weights using gradient descent; w1.data are Tensors,
         # w.grad are Variables and w.grad.data are Tensors.
-        W.data -= eta * W.grad.data
+        for W in Ws:
+            W.data -= eta * W.grad.data
         #
         if i % 500 == 0 or i == 0:
             current_loss = loss.data.numpy()[0]
@@ -139,9 +172,15 @@ def main(argv=None):
                 print('loss: ',current_loss)
                 break
         # Manually zero the gradients after updating weights
-        W.grad.data.zero_()
+        for W in Ws:
+            W.grad.data.zero_()
+        for i in range(len(Ws)):
+            W = Ws[i]
+            W_avg = W_avgs[i]
+            W_avg = (1/nb_iter)*W + W_avg
     #
-    c_sgd = W.data.numpy()
+    #c_avg = W_avg.data.numpy()
+    #c_sgd = W.data.numpy()
     X_mdl = X_mdl.data.numpy()
     y = y.data.numpy()
     #
@@ -149,29 +188,61 @@ def main(argv=None):
     print('Degree_mdl = {}, N = {}, M = {}, eta = {}, nb_iter = {}'.format(Degree_mdl,N,M,eta,nb_iter))
     #
     print('\n---- statistics about learned params')
-    print('||c_sgd - c_pinv|| = ', np.linalg.norm(c_sgd - c_pinv,2))
+    #print('||c_sgd - c_pinv|| = ', np.linalg.norm(c_sgd - c_pinv,2))
+    #print('||c_sgd - c_avg|| = ', np.linalg.norm(c_sgd - c_pinv,2))
+    #print('||c_avg - c_pinv|| = ', np.linalg.norm(c_avg - c_pinv,2))
+    # def f_avg(x):
+    #     X = poly_kernel_matrix( [x],D_sgd-1 )
+    #     return np.dot(X,c_avg)
+    def f_avg(x):
+        W_l1 = W_avgs[0]
+        W_l2 = W_avgs[1]
+        W_out = W_avgs[2]
+        x = torch.FloatTensor([x])
+        x = Variable(torch.FloatTensor(x).type(dtype))
+        x = x.view(1,1)
+        a_l1 = x.mm(W_l1)**2 # [M,H^(1)] = [M,D]x[D,H^(1)]
+        a_l2 = a_l1.mm(W_l2)**2 # [M,H^(2)] = [M,H^(1)]x[H^(1),H^(2)]
+        y_pred = a_l2.mm(W_out) # [M,1] = [M,H^(2)]x[M^(2),1]
+        return y_pred.data.numpy()
+    # def f_sgd(x):
+    #     X = poly_kernel_matrix( [x],D_sgd-1 )
+    #     return np.dot(X,c_sgd)
     def f_sgd(x):
-        X = poly_kernel_matrix( [x],D_sgd-1 )
-        return np.dot(X,c_sgd)
+        W_l1 = Ws[0]
+        W_l2 = Ws[1]
+        W_out = Ws[2]
+        #pdb.set_trace()
+        x = torch.FloatTensor([x])
+        x = Variable(torch.FloatTensor(x).type(dtype))
+        x = x.view(1,1)
+        a_l1 = x.mm(W_l1)**2 # [M,H^(1)] = [M,D]x[D,H^(1)]
+        a_l2 = a_l1.mm(W_l2)**2 # [M,H^(2)] = [M,H^(1)]x[H^(1),H^(2)]
+        y_pred = a_l2.mm(W_out) # [M,1] = [M,H^(2)]x[M^(2),1]
+        return y_pred.data.numpy()
     def g_pinv(x):
         X = poly_kernel_matrix( [x],D_sgd-1 )
         return np.dot(X,c_pinv)
-    print('||f_sgd - f_pin||^2 = ', L2_norm_2(f=f_sgd,g=g_pinv,lb=0,ub=1))
+    print('||f_sgd - f_pinv||^2 = ', L2_norm_2(f=f_sgd,g=g_pinv,lb=0,ub=1))
+    print('||f_avg - f_pinv||^2 = ', L2_norm_2(f=f_avg,g=g_pinv,lb=0,ub=1))
     #
-    print('c_sgd.shape: ', c_sgd.shape)
+    #print('c_sgd.shape: ', c_sgd.shape)
     print('c_pinv.shape: ', c_pinv.shape)
     print('c_rls.shape: ', c_rls.shape)
-    print('norm(c_sgd): ', np.linalg.norm(c_sgd))
+    #print('norm(c_sgd): ', np.linalg.norm(c_sgd))
     print('norm(c_pinv): ', np.linalg.norm(c_pinv))
     print('norm(c_rls): ', np.linalg.norm(c_rls))
 
     #
-    Xc_pinv = np.dot(X_mdl,c_sgd)
-    print(' J(c_sgd) = ', (1/N)*(np.linalg.norm(y-Xc_pinv)**2) )
-    Xc_pinv = np.dot(X_mdl,c_pinv)
+    #Xc_sdg = np.dot(X_mdl,c_sgd)
+    #print(' J(c_sgd) = ', (1/N)*(np.linalg.norm(y-Xc_sdg)**2) )
+    print(' J(c_sgd) = ', (1/N)*(y_pred - batch_ys).pow(2).sum().data.numpy() )
+    Xc_pinv = np.dot( poly_kernel_matrix( x_true,D_sgd-1 ),c_pinv)
     print( ' J(c_pinv) = ',(1/N)*(np.linalg.norm(y-Xc_pinv)**2) )
-    Xc_rls = np.dot(X_mdl,c_rls)
-    print( ' J(c_rls) = ',(1/N)*(np.linalg.norm(y-Xc_rls)**2) )
+    #Xc_rls = np.dot(X_mdl,c_rls)
+    #print( ' J(c_rls) = ',(1/N)*(np.linalg.norm(y-Xc_rls)**2) )
+    #Xc_W_avg = np.dot(X_mdl,c_avg)
+    #print( ' J(c_avg) = ',(1/N)*(np.linalg.norm(y-Xc_W_avg)**2) )
     #
     seconds = (time.time() - start_time)
     minutes = seconds/ 60
@@ -184,17 +255,22 @@ def main(argv=None):
     x_horizontal = np.linspace(lb,ub,1000)
     X_plot = poly_kernel_matrix(x_horizontal,D_sgd-1)
     #plots
-    p_sgd, = plt.plot(x_horizontal, np.dot(X_plot,c_sgd))
+    #p_sgd, = plt.plot(x_horizontal, np.dot(X_plot,c_sgd))
+    p_sgd, = plt.plot(x_horizontal, [ float(f_sgd(x_i)[0]) for x_i in x_horizontal ])
     p_pinv, = plt.plot(x_horizontal, np.dot(X_plot,c_pinv))
     #p_rls, = plt.plot(x_horizontal, np.dot(X_plot,c_rls))
+    #p_avg, = plt.plot(x_horizontal, np.dot(X_plot,c_avg))
+    p_avg, = plt.plot(x_horizontal, [ float(f_avg(x_i)[0]) for x_i in x_horizontal ])
     p_data, = plt.plot(x_true,y,'ro')
     #
     #p_list=[p_sgd,p_pinv,p_rls,p_data]
     #p_list=[p_data]
-    p_list=[p_sgd,p_pinv,p_data]
+    #p_list=[p_sgd,p_pinv,p_data]
+    p_list = [p_avg,p_sgd,p_pinv,p_data]
     #p_list=[p_pinv,p_data]
     #plt.legend(p_list,['sgd curve Degree_mdl='+str(D_sgd-1),'min norm (pinv) Degree_mdl='+str(D_pinv-1),'rls regularization lambda={} Degree_mdl={}'.format(lambda_rls,D_rls-1),'data points'])
-    plt.legend(p_list,['sgd curve Degree_mdl={}, batch-size= {}, iterations={}, eta={}'.format(str(D_sgd-1),M,nb_iter,eta),'min norm (pinv) Degree_mdl='+str(D_pinv-1),'data points'])
+    #plt.legend(p_list,['sgd curve Degree_mdl={}, batch-size= {}, iterations={}, eta={}'.format(str(D_sgd-1),M,nb_iter,eta),'min norm (pinv) Degree_mdl='+str(D_pinv-1),'data points'])
+    plt.legend(p_list,['average sgd model Degree_mdl={}'.format( str(D_sgd-1) ),'sgd curve Degree_mdl={}, batch-size= {}, iterations={}, eta={}'.format(str(D_sgd-1),M,nb_iter,eta),'min norm (pinv) Degree_mdl='+str(D_pinv-1),'data points'])
     #plt.legend(p_list,['min norm (pinv) Degree_mdl='+str(D_pinv-1),'data points'])
     plt.ylabel('f(x)')
     ##
@@ -210,7 +286,17 @@ def main(argv=None):
     ##
     plt.show()
 
+def plot_pts():
+    fig2 = plt.figure()
+    p_grads, = plt.plot([200,375,500], [0.45283,0.1125,0.02702],color='g')
+    plt.legend([p_grads],['L2 norm'])
+    plt.title('How SGD approaches pseudoinverse')
+    plt.xlabel('iterations (thousands)')
+    plt.ylabel('L2 norm between SGD solution and pinv')
+    plt.show()
+
 if __name__ == '__main__':
     #tf.app.run()
+    #plot_pts()
     main()
     print('\a')
