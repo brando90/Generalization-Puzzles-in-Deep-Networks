969fc2a097b057b5eb21a0d649b65e604fb32472
Brando Miranda <brando90@mit.edu>
2017-11-06 13:49:31 -0500

    its running

--- pytorch_experiments/one_SGD_expt_WP.py
+++ pytorch_experiments/one_SGD_expt_WP.py
@@ -84,6 +84,7 @@ def get_hp_to_run(hyper_params,repetitions,satid):
     raise ValueError('There is something wrong with the number of jobs you submitted compared.')
 
 def main(**kwargs):
+    WP=False
     ##
     start_time = time.time()
     np.set_printoptions(suppress=True) #Whether or not suppress printing of small floating point values using scientific notation (default False).
@@ -99,6 +100,7 @@ def main(**kwargs):
     truth_filename='data_gen_type_mdl=WP_D_layers_[3, 1, 1]_nb_layers3_bias[None, True, False]_mu0.0_std5.0_N_train_8_N_test_20_lb_-1_ub_1_act_poly_act_degree2_nb_params_5_msg_'
     data_filename='data_numpy_type_mdl=WP_D_layers_[3, 1, 1]_nb_layers3_bias[None, True, False]_mu0.0_std5.0_N_train_8_N_test_20_lb_-1_ub_1_act_poly_act_degree2_nb_params_5_msg_.npz'
     ##
+    truth_filename=''
     data_filename='degree4_fit_2_sin.npz'
     #experiment_name = 'unit_test'
     #experiment_name = 'linear_unit_test'
@@ -117,6 +119,7 @@ def main(**kwargs):
     lb,ub = 0.01,10000
     one_over_lambdas = np.linspace(lb,ub,N_lambdas)
     lambdas = list( 1/one_over_lambdas )
+    lambdas = N_lambdas*[0]
     nb_iterations = [int(1.4*10**6)]
     #nb_iterations = [int(8*10**4)]
     nb_iterations = [int(60*100)]
@@ -150,7 +153,7 @@ def main(**kwargs):
     print('reg_lambda_WP = ',reg_lambda_WP)
     print('nb_iter = ',nb_iter)
     #### Get Data set
-    if truth_filename is not None:
+    if truth_filename != '':
         mdl_truth_dict = torch.load('./data/'+truth_filename)
         D_layers_truth=extract_list_filename(truth_filename)
     ## load data
@@ -160,70 +163,80 @@ def main(**kwargs):
     X_test, Y_test = data['X_test'], data['Y_test']
     D_data = X_test.shape[1]
     ## get nb data points
+    D0 = D_data
     N_train,_ = X_train.shape
     N_test,_ = X_test.shape
     ## activation function
-    adegree = 2
-    ax = np.concatenate( (np.linspace(-20,20,100), np.linspace(-10,10,1000)) )
-    aX = np.concatenate( (ax,np.linspace(-2,2,100000)) )
-    act, c_pinv_relu = get_relu_poly_act2(aX,degree=adegree) # ax**2+bx+c, #[1, x^1, ..., x^D]
-    print('c_pinv_relu = ', c_pinv_relu)
-    #act = relu
-    #act = lambda x: x
-    #act.__name__ = 'linear'
-    # plot_activation_func(act,lb=-20,ub=20,N=1000)
-    # plt.show()
-    #### 2-layered mdl
-    D0 = D_data
+    if WP:
+        adegree = 2
+        ax = np.concatenate( (np.linspace(-20,20,100), np.linspace(-10,10,1000)) )
+        aX = np.concatenate( (ax,np.linspace(-2,2,100000)) )
+        act, c_pinv_relu = get_relu_poly_act2(aX,degree=adegree) # ax**2+bx+c, #[1, x^1, ..., x^D]
+        print('c_pinv_relu = ', c_pinv_relu)
+        #act = relu
+        #act = lambda x: x
+        #act.__name__ = 'linear'
+        # plot_activation_func(act,lb=-20,ub=20,N=1000)
+        # plt.show()
+        #### 2-layered mdl
 
-    H1 = 12
-    D0,D1,D2 = D0,H1,1
-    D_layers,act = [D0,D1,D2], act
+        H1 = 12
+        D0,D1,D2 = D0,H1,1
+        D_layers,act = [D0,D1,D2], act
 
-    # H1,H2 = 20,20
-    # D0,D1,D2,D3 = D0,H1,H2,1
-    # D_layers,act = [D0,D1,D2,D3], act
+        # H1,H2 = 20,20
+        # D0,D1,D2,D3 = D0,H1,H2,1
+        # D_layers,act = [D0,D1,D2,D3], act
 
-    # H1,H2,H3 = 15,15,15
-    # D0,D1,D2,D3,D4 = D0,H1,H2,H3,1
-    # D_layers,act = [D0,D1,D2,D3,D4], act
+        # H1,H2,H3 = 15,15,15
+        # D0,D1,D2,D3,D4 = D0,H1,H2,H3,1
+        # D_layers,act = [D0,D1,D2,D3,D4], act
 
-    # H1,H2,H3,H4 = 25,25,25,25
-    # D0,D1,D2,D3,D4,D5 = D0,H1,H2,H3,H4,1
-    # D_layers,act = [D0,D1,D2,D3,D4,D5], act
+        # H1,H2,H3,H4 = 25,25,25,25
+        # D0,D1,D2,D3,D4,D5 = D0,H1,H2,H3,H4,1
+        # D_layers,act = [D0,D1,D2,D3,D4,D5], act
 
-    nb_layers = len(D_layers)-1 #the number of layers include the last layer (the regression layer)
-    biases = [None] + [True] + (nb_layers-1)*[False] #bias only in first layer
-    #biases = [None] + (nb_layers)*[True] # biases in every layer
-    ## mdl degree and D
-    nb_hidden_layers = nb_layers-1 #note the last "layer" is a summation layer for regression and does not increase the degree of the polynomial
-    Degree_mdl = adegree**( nb_hidden_layers ) # only hidden layers have activation functions
-    ## Lift data/Kernelize data
-    poly_feat = PolynomialFeatures(degree=Degree_mdl)
-    Kern_train, Kern_test = poly_feat.fit_transform(X_train), poly_feat.fit_transform(X_test)
-    ## LA models
-    if D0 == 1:
-        Degree_mdl = 4 ## variable to CHANGE
-        c_pinv = np.polyfit( X_train.reshape((N_train,)) , Y_train.reshape((N_train,)) , Degree_mdl )[::-1]
+        nb_layers = len(D_layers)-1 #the number of layers include the last layer (the regression layer)
+        biases = [None] + [True] + (nb_layers-1)*[False] #bias only in first layer
+        #biases = [None] + (nb_layers)*[True] # biases in every layer
+        ## mdl degree and D
+        nb_hidden_layers = nb_layers-1 #note the last "layer" is a summation layer for regression and does not increase the degree of the polynomial
+        Degree_mdl = adegree**( nb_hidden_layers ) # only hidden layers have activation functions
+        ## Lift data/Kernelize data
+        poly_feat = PolynomialFeatures(degree=Degree_mdl)
+        Kern_train, Kern_test = poly_feat.fit_transform(X_train), poly_feat.fit_transform(X_test)
+        ## LA models
+        if D0 == 1:
+            c_pinv = np.polyfit( X_train.reshape((N_train,)) , Y_train.reshape((N_train,)) , Degree_mdl )[::-1]
+        else:
+            ## TODO: https://stackoverflow.com/questions/10988082/multivariate-polynomial-regression-with-numpy
+            c_pinv = np.dot(np.linalg.pinv( Kern_train ),Y_train)
+        ## inits
+        init_config = Maps( {'w_init':'w_init_normal','mu':0.0,'std':0.01, 'bias_init':'b_fill','bias_value':0.01,'biases':biases ,'nb_layers':len(D_layers)} )
+        w_inits_sgd, b_inits_sgd = get_initialization(init_config)
+        init_config_standard_sgd = Maps( {'mu':0.0,'std':0.001, 'bias_value':0.01} )
+        mdl_stand_initializer = lambda mdl: lifted_initializer(mdl,init_config_standard_sgd)
+        ## SGD models
+        if truth_filename:
+            mdl_truth = NN(D_layers=D_layers_truth,act=act,w_inits=w_inits_sgd,b_inits=b_inits_sgd,biases=biases)
+            mdl_truth.load_state_dict(mdl_truth_dict)
+        mdl_sgd = NN(D_layers=D_layers,act=act,w_inits=w_inits_sgd,b_inits=b_inits_sgd,biases=biases)
+        ## data to TORCH
+        data = get_data_struct(X_train,Y_train,X_test,Y_test,Kern_train,Kern_test,dtype)
     else:
-        ## TODO: https://stackoverflow.com/questions/10988082/multivariate-polynomial-regression-with-numpy
-        c_pinv = np.dot(np.linalg.pinv( Kern_train ),Y_train)
-    ## inits
-    init_config = Maps( {'w_init':'w_init_normal','mu':0.0,'std':0.01, 'bias_init':'b_fill','bias_value':0.01,'biases':biases ,'nb_layers':len(D_layers)} )
-    w_inits_sgd, b_inits_sgd = get_initialization(init_config)
-    init_config_standard_sgd = Maps( {'mu':0.0,'std':0.001, 'bias_value':0.01} )
-    mdl_stand_initializer = lambda mdl: lifted_initializer(mdl,init_config_standard_sgd)
-    ## SGD models
-    if truth_filename:
-        mdl_truth = NN(D_layers=D_layers_truth,act=act,w_inits=w_inits_sgd,b_inits=b_inits_sgd,biases=biases)
-        mdl_truth.load_state_dict(mdl_truth_dict)
-    mdl_sgd = NN(D_layers=D_layers,act=act,w_inits=w_inits_sgd,b_inits=b_inits_sgd,biases=biases)
-    ##
-    mdl_sgd = get_sequential_lifted_mdl(nb_monomials=c_pinv.shape[0],D_out=1, bias=False)
-    ## data to TORCH
-    data = get_data_struct(X_train,Y_train,X_test,Y_test,Kern_train,Kern_test,dtype)
-    data_stand = get_data_struct(X_train,Y_train,X_test,Y_test,Kern_train,Kern_test,dtype)
-    data_stand.X_train, data_stand.X_test = data_stand.Kern_train, data_stand.Kern_test
+        Degree_mdl = 4
+        ## Lift data/Kernelize data
+        poly_feat = PolynomialFeatures(degree=Degree_mdl)
+        Kern_train, Kern_test = poly_feat.fit_transform(X_train), poly_feat.fit_transform(X_test)
+        ## LA models
+        if D0 == 1:
+            c_pinv = np.polyfit( X_train.reshape((N_train,)) , Y_train.reshape((N_train,)) , Degree_mdl )[::-1]
+        else:
+            ## TODO: https://stackoverflow.com/questions/10988082/multivariate-polynomial-regression-with-numpy
+            c_pinv = np.dot(np.linalg.pinv( Kern_train ),Y_train)
+        mdl_sgd = get_sequential_lifted_mdl(nb_monomials=c_pinv.shape[0],D_out=1, bias=False)
+        data = get_data_struct(X_train,Y_train,X_test,Y_test,Kern_train,Kern_test,dtype)
+        data.X_train, data.X_test = data.Kern_train, data.Kern_test
     ## check number of monomials
     nb_monomials = int(scipy.misc.comb(D0+Degree_mdl,Degree_mdl))
     nb_terms = c_pinv.shape[0]
@@ -247,8 +260,11 @@ def main(**kwargs):
         except Exception as e:
             err_msg = str(e)
             print(f'\nException caught during training with msg: {err_msg}')
-            w_inits_sgd, b_inits_sgd = get_initialization(init_config)
-            mdl_sgd = NN(D_layers=D_layers,act=act,w_inits=w_inits_sgd,b_inits=b_inits_sgd,biases=biases)
+            if WP:
+                w_inits_sgd, b_inits_sgd = get_initialization(init_config)
+                mdl_sgd = NN(D_layers=D_layers,act=act,w_inits=w_inits_sgd,b_inits=b_inits_sgd,biases=biases)
+            else:
+                mdl_sgd = get_sequential_lifted_mdl(nb_monomials=c_pinv.shape[0],D_out=1, bias=False)
     ##
     train_error_WP = (1/N_train)*(mdl_sgd.forward(data.X_train) - data.Y_train).pow(2).sum().data.numpy()
     test_error_WP = (1/N_test)*(mdl_sgd.forward(data.X_test) - Variable(torch.FloatTensor(Y_test)) ).pow(2).sum().data.numpy()
