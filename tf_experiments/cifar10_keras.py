2c9f1f1bca1e11452a3c50f2a90201d8225828c2
Brando Miranda <brando90@mit.edu>
2018-03-13 22:20:48 -0400

    keras GPU

---
+++ tf_experiments/cifar10_keras.py
@@ -0,0 +1,88 @@
+# Import all modules
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.constraints import maxnorm
+from keras.optimizers import SGD
+from keras.layers import Activation
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.normalization import BatchNormalization
+from keras.utils import np_utils
+#from keras_sequential_ascii import sequential_model_to_ascii_printout
+from keras import backend as K
+
+if K.backend()=='tensorflow':
+    K.set_image_dim_ordering("th")
+
+# Import Tensorflow with multiprocessing
+import tensorflow as tf
+import multiprocessing as mp
+
+from data_sets import load_cifar10
+from nn_models import model_convs_FCs
+from nn_models import compile_mdl_with_sgd
+
+## https://blog.plon.io/tutorials/cifar-10-classification-using-keras-tutorial/#comment-670
+
+def main():
+    ''' experiment type '''
+    expt = 'BoixNet'
+    #expt = 'LiaoNet'
+    ''' declare variables '''
+    batch_size = 32
+    num_classes = 10
+    nb_epochs = 2
+    lr = 0.01
+    ''' load cifar '''
+    (x_train, y_train), (x_test, y_test) = load_cifar10(num_classes)
+    ''' params for cnn'''
+    if expt == 'BoixNet':
+        nb_conv_layers, nb_fc_layers = 2,3
+        nb_conv_filters = [32]*nb_conv_layers
+        kernels = [(5,5)]*nb_conv_layers
+        nb_units_fcs = [512,256,num_classes]
+    elif expt == 'LiaoNet':
+        nb_conv_layers, nb_fc_layers = 5,1
+        nb_conv_filters = [32]*nb_conv_layers
+        kernels = [(5,5)]*nb_conv_layers
+        nb_units_fcs = [512,num_classes]
+    CHW = x_train.shape[1:] # (3, 32, 32)
+    ''' get model '''
+    cnn_n = model_convs_FCs(CHW, nb_conv_layers,nb_fc_layers, nb_conv_filters,kernels, nb_units_fcs)
+    cnn_n.summary()
+    compile_mdl_with_sgd(cnn_n,lr, weight_decay=0,momentum=0,nesterov=False)
+    ''' Fit model '''
+    cnn = cnn_n.fit(x_train, y_train, batch_size=batch_size, epochs=nb_epochs, validation_data=(x_test,y_test),shuffle=True)
+    ''' Plots '''
+    # Plots for training and testing process: loss and accuracy
+    plt.figure(0)
+    plt.plot(cnn.history['acc'],'r')
+    plt.plot(cnn.history['val_acc'],'g')
+    plt.xticks(np.arange(0, 101, 2.0))
+    plt.rcParams['figure.figsize'] = (8, 6)
+    plt.xlabel("Num of Epochs")
+    plt.ylabel("Accuracy")
+    plt.title("Training Accuracy vs Validation Accuracy")
+    plt.legend(['train','validation'])
+
+
+    plt.figure(1)
+    plt.plot(cnn.history['loss'],'r')
+    plt.plot(cnn.history['val_loss'],'g')
+    plt.xticks(np.arange(0, 101, 2.0))
+    plt.rcParams['figure.figsize'] = (8, 6)
+    plt.xlabel("Num of Epochs")
+    plt.ylabel("Loss")
+    plt.title("Training Loss vs Validation Loss")
+    plt.legend(['train','validation'])
+
+if __name__ == '__main__':
+    main()
+
+
+plt.show()
---
+++ tf_experiments/data_sets.py
@@ -0,0 +1,15 @@
+# Loading the CIFAR-10 datasets
+from keras.datasets import cifar10
+
+from keras.utils import np_utils
+
+def load_cifar10(num_classes):
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    # x_train - training data(images), y_train - labels(digits)
+    y_train = np_utils.to_categorical(y_train, num_classes)
+    y_test = np_utils.to_categorical(y_test, num_classes)
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+    x_train  /= 255
+    x_test /= 255
+    return (x_train, y_train), (x_test, y_test)
