ee01a353b30b218828870a1fc7a2cdf79c7d4206
Brando Miranda <brando90@mit.edu>
2018-03-15 15:02:11 -0400

    standardize [-1,1]

--- tf_experiments/data_sets.py
+++ tf_experiments/data_sets.py
@@ -3,7 +3,7 @@ from keras.datasets import cifar10
 
 from keras.utils import np_utils
 
-def load_cifar10(num_classes):
+def load_cifar10(num_classes,standardize=False):
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
     # x_train - training data(images), y_train - labels(digits)
     y_train = np_utils.to_categorical(y_train, num_classes)
@@ -12,4 +12,8 @@ def load_cifar10(num_classes):
     x_test = x_test.astype('float32')
     x_train  /= 255
     x_test /= 255
+    ''' '''
+    if standardize:
+        x_train = (x_train - 0.5)/0.5
+        x_test = (x_test - 0.5)/0.5
     return (x_train, y_train), (x_test, y_test)
