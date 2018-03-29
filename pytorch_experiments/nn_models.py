373f53f2dfb182290ae5cd766734cde427f9ecdc
Brando Miranda <brando90@mit.edu>
2018-03-29 16:05:13 -0400

    BoixNet2

--- pytorch_experiments/nn_models.py
+++ pytorch_experiments/nn_models.py
@@ -77,6 +77,47 @@ class BoixNet(nn.Module):
         self.fc2 = nn.Linear(nb_units_fc1,nb_units_fc2)
         if self.do_bn: self.fc2_bn = nn.BatchNorm1d(nb_units_fc2)
         self.fc3 = nn.Linear(nb_units_fc2,nb_units_fc3)
+        if self.do_bn: self.fc3_bn = nn.BatchNorm1d(nb_units_fc3) #layer right before output no BN
+
+    def forward(self, x):
+        ''' conv layers'''
+        pre_act1 = self.bn_conv1(self.conv1(x)) if self.do_bn else self.conv1(x)
+        a_conv1 = F.relu(pre_act1)
+        ##
+        pre_act2 = self.bn_conv2(self.conv2(a_conv1)) if self.do_bn else self.conv2(a_conv1)
+        a_conv2 = F.relu(pre_act2)
+        ''' FC layers '''
+        _,C,H,W = a_conv2.size()
+        a_flat_conv2 = a_conv2.view(-1,C*H*W)
+        ##
+        pre_act_fc1 = self.fc1_bn(self.fc1(a_flat_conv2)) if self.do_bn else self.fc1(a_flat_conv2)
+        a_fc1 = F.relu(pre_act_fc1)
+        pre_act_fc2 = self.fc2_bn(self.fc2(a_fc1)) if self.do_bn else self.fc2(a_fc1)
+        a_fc2 = F.relu(pre_act_fc2)
+        pre_act_fc3 = self.fc3(a_fc2)
+        a_fc3 = pre_act_fc3
+        return a_fc3
+
+class BoixNet2(nn.Module):
+    ## The network has 2 convolutional layers followed by 3 fully connected.
+    ## Use ReLUs, and no batch normalization or regularizers.
+    ## Trained with cross-entropy
+    ## https://discuss.pytorch.org/t/when-creating-new-neural-net-from-scratch-how-does-one-statically-define-what-the-size-of-the-a-flatten-layer-is-not-at-runtime/14235
+    def __init__(self,C,H,W, nb_filters1,nb_filters2, kernel_size1,kernel_size2, nb_units_fc1,nb_units_fc2,nb_units_fc3,do_bn=False):
+        super(BoixNet, self).__init__()
+        self.do_bn = do_bn
+        ''' Initialize conv layers'''
+        self.conv1 = nn.Conv2d(3,nb_filters1, kernel_size1) #(in_channels, out_channels, kernel_size)
+        if self.do_bn: self.bn_conv1 = nn.BatchNorm2d(nb_filters1)
+        self.conv2 = nn.Conv2d(nb_filters1,nb_filters2, kernel_size2)
+        if self.do_bn: self.bn_conv2 = nn.BatchNorm2d(nb_filters2)
+        CHW = ((H-kernel_size1+1)-kernel_size2+1) * ((W-kernel_size1+1)-kernel_size2+1) * nb_filters2
+        ''' '''
+        self.fc1 = nn.Linear(CHW, nb_units_fc1)
+        if self.do_bn: self.fc1_bn = nn.BatchNorm1d(nb_units_fc1)
+        self.fc2 = nn.Linear(nb_units_fc1,nb_units_fc2)
+        if self.do_bn: self.fc2_bn = nn.BatchNorm1d(nb_units_fc2)
+        self.fc3 = nn.Linear(nb_units_fc2,nb_units_fc3)
         #if self.do_bn: self.fc3_bn = nn.BatchNorm1d(nb_units_fc3) #layer right before output no BN
 
     def forward(self, x):
