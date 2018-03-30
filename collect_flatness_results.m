5e2775a943e28032b6cb69f6c73664b35319c98f
Brando Miranda <brando90@mit.edu>
2018-03-30 16:30:31 -0400

    flatness collect results

---
+++ collect_flatness_results.m
@@ -0,0 +1,61 @@
+clc;clear;
+%% script folder
+dot = '/Users/brandomiranda/home_simulation_research/overparametrized_experiments/';
+cd(dot);
+%% Get perturbation errors for natural label experiment
+path_natural_label='/pytorch_experiments/test_runs_flatness/flatness_29_March_label_corrupt_prob_0.0_exptlabel_pert_retrained_Random_Label_on_Natural_Label_BoixNet';
+expt_path = fullfile(dot,path_natural_label);
+results_natural_label = get_results(expt_path);
+%% Get perturbation errors for random label experiment
+path_random_label='/pytorch_experiments/test_runs_flatness/flatness_29_March_label_corrupt_prob_0.0_exptlabel_pert_Natural_Label_BoixNet';
+expt_path = fullfile(dot,path_natural_label);
+results_random_label = get_results(expt_path);
+%% Sort the results
+maps2sortedArray(results_random_label.M_tests_accs)
+%% return to script folder
+cd(dot)
+%% Helper function
+function results = get_results(expt_path)
+    results = struct();
+    M_tests_accs = containers.Map('KeyType','double','ValueType','any');
+    M_train_accs = containers.Map('KeyType','double','ValueType','any');
+    M_tests_losses = containers.Map('KeyType','double','ValueType','any');
+    M_train_losses = containers.Map('KeyType','double','ValueType','any');
+    %%
+    cd(expt_path)
+    files = dir('noise*');
+    directoryNames = {files([files.isdir]).name};
+    for expt_dir_name = directoryNames
+        name_expt_dir = expt_dir_name{1}
+        noise = str2double(name_expt_dir(7:end)) % extract the noise from the name
+        %%
+        all_test_accs = [];
+        all_train_accs = [];
+        all_test_losses = [];
+        all_train_lossses = [];
+        %%
+        noise_dir_path = fullfile(name_expt_dir,'noise*');
+        files = dir(noise_dir_path);
+        files = {files.name};
+        for expt_file_name = files
+            noise_matlab_file = fullfile(name_expt_dir,expt_file_name{1});
+            load(noise_matlab_file);
+            all_test_accs = [all_test_accs, test_accs];
+            all_train_accs = [all_train_accs, train_accs];
+            all_test_losses = [all_test_losses, test_losses];
+            all_train_lossses = [all_train_lossses, train_losses];
+        end
+        M_tests_accs(noise) = all_test_accs;
+        M_train_accs(noise) = all_train_accs;
+        M_tests_losses(noise) = all_test_losses;
+        M_train_losses(noise) = all_train_lossses;
+    end
+    results.M_tests_accs = M_tests_accs;
+    results.M_train_accs = M_train_accs;
+    results.M_tests_losses = M_tests_losses;
+    results.M_train_losses = M_train_losses;
+end
+
+function maps2sortedArray(M)
+    M
+end
