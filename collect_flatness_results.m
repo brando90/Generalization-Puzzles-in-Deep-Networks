5022411e9289127ec32f4018c82ff7ec48ba7733
Brando Miranda <brando90@mit.edu>
2018-03-30 16:58:39 -0400

    flatness collec results

--- collect_flatness_results.m
+++ collect_flatness_results.m
@@ -10,8 +10,14 @@ results_natural_label = get_results(expt_path);
 path_random_label='/pytorch_experiments/test_runs_flatness/flatness_29_March_label_corrupt_prob_0.0_exptlabel_pert_Natural_Label_BoixNet';
 expt_path = fullfile(dot,path_natural_label);
 results_random_label = get_results(expt_path);
-%% Sort the results
-maps2sortedArray(results_random_label.M_tests_accs)
+%% Plot Results for random label
+noises = cell2list( keys(results_random_label.M_tests_accs) );
+[means_test, stds_test] = get_stats_experiment(results_random_label.M_tests_accs);
+[means_train, stds_train] = get_stats_experiment(results_random_label.M_train_accs);
+figure;
+errorbar(noises,means_test,stds_test)
+figure;
+errorbar(noises,means_train,stds_train)
 %% return to script folder
 cd(dot)
 %% Helper function
@@ -26,8 +32,8 @@ function results = get_results(expt_path)
     files = dir('noise*');
     directoryNames = {files([files.isdir]).name};
     for expt_dir_name = directoryNames
-        name_expt_dir = expt_dir_name{1}
-        noise = str2double(name_expt_dir(7:end)) % extract the noise from the name
+        name_expt_dir = expt_dir_name{1};
+        noise = str2double(name_expt_dir(7:end)); % extract the noise from the name
         %%
         all_test_accs = [];
         all_train_accs = [];
@@ -55,7 +61,22 @@ function results = get_results(expt_path)
     results.M_tests_losses = M_tests_losses;
     results.M_train_losses = M_train_losses;
 end
-
-function maps2sortedArray(M)
-    M
+function [means, stds] = get_stats_experiment(M)
+    noises = keys(M);
+    vals = values(M);
+    means = length(noises);
+    stds = length(noises);
+    for i=length(noises)
+        mu = mean(vals{1});
+        stddev = std(vals{1});
+        means(i) = mu;
+        stds(i) = stddev;
+    end
+end
+function list = cell2list(M)
+    list = zeros(1,length(M));
+    for i = 1:length(M)
+        m = M(i);
+        list(i) = m{1};
+    end
 end
