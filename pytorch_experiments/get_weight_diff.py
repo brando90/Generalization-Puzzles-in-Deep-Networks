ebbacbe601d747bbd6144528d2abb31ae78853e0
Brando Miranda <brando90@mit.edu>
2018-04-03 18:46:49 -0400

    diff of nets

--- pytorch_experiments/get_weight_diff.py
+++ pytorch_experiments/get_weight_diff.py
@@ -11,8 +11,8 @@ results_root = './test_runs_flatness'
 path_nl = os.path.join(results_root,'flatness_28_March_label_corrupt_prob_0.0_exptlabel_BoixNet_polestar_300_stand_natural_labels/net_28_March_206')
 net_nl = torch.load(path_nl)
 # RLNL
-path_rl_nl = os.path.join(results_root,'flatness_28_March_label_corrupt_prob_0.0_exptlabel_re_train_RLBoixNet_noBN_polestar_150/net_28_March_18')
-net_rlnl = orch.load(path_rlnl)
+path_rlnl = os.path.join(results_root,'flatness_28_March_label_corrupt_prob_0.0_exptlabel_re_train_RLBoixNet_noBN_polestar_150/net_28_March_18')
+net_rlnl = torch.load(path_rlnl)
 ''' compute weight diff '''
-total_norm = utils.weight_diff_btw_nets(net1,net2)
+total_norm = utils.weight_diff_btw_nets(net_nl,net_rlnl)
 print('||W1 - W2||')
