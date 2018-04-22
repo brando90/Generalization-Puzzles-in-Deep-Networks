91a7db52be7c1951b8f11e40bf265ca5e95e8949
Brando Miranda <brando90@mit.edu>
2018-04-22 00:24:59 -0400

    sharpness extractor code

--- pytorch_experiments/sharpness_data_lib.py
+++ pytorch_experiments/sharpness_data_lib.py
@@ -25,6 +25,15 @@ def save_data_according_to_criterion(dataloader,criterion):
     with open(path,'wb+') as array_file: #wb+ Opens a file for both writing and reading in binary format. Overwrites the existing file if the file exists. If the file does not exist, creates a new file for reading and writing.
         pickle.dump(sorted_by_second, array_file)
 
+def other():
+    cifar_dataset = torchvision.datasets.CIFAR10(root='./data', transform=transform)
+    train_indices =  # select train indices according to your rule
+    test_indices =  # select test indices according to your rule
+    train_loader = torch.utils.data.DataLoader(cifar_dataset, batch_size=32, shuffle=True,
+                                               sampler=SubsetRandomSampler(train_indices))
+    test_loader = torch.utils.data.DataLoader(cifar_dataset, batch_size=32, shuffle=True,
+                                              sampler=SubsetRandomSampler(test_indices))
+
 def main():
     ''' get data loaders '''
     #TODO
