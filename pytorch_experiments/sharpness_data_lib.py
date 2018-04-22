a1d218440f1d6e8f9ab3fef302bc5ac08366c4c7
Brando Miranda <brando90@mit.edu>
2018-04-22 00:22:18 -0400

    sharpness extractor code

---
+++ pytorch_experiments/sharpness_data_lib.py
@@ -0,0 +1,34 @@
+import torch
+
+import pickle
+
+import utils
+
+def save_data_according_to_criterion(dataloader,criterion):
+    '''
+
+    :param dataloader:
+    :param criteron:
+    :return:
+    '''
+    ''' produce scores list  [(i,score)] '''
+    enable_cuda = False
+    index_scores = []
+    for i,data_train in enumerate(dataloader):
+        inputs, labels = utils.extract_data(enable_cuda,data_train,wrap_in_variable=False)
+        score = criterion(inputs)
+        index_scores.append( (i,score) )
+    ''' sort scores list based on scores '''
+    sorting_criterion = lambda tup: tup[1]
+    sorted_by_second = sorted(index_scores, key=sorting_criterion)
+    ''' pickle the index,score sorted array to '''
+    with open(path,'wb+') as array_file: #wb+ Opens a file for both writing and reading in binary format. Overwrites the existing file if the file exists. If the file does not exist, creates a new file for reading and writing.
+        pickle.dump(sorted_by_second, array_file)
+
+def main():
+    ''' get data loaders '''
+    #TODO
+    ''' load net for the cirterion '''
+    # TODO
+    ''' '''
+    save_data_according_to_criterion(dataloader,citerion)
