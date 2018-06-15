82e3721a4656c09bd70de57c675e0dcf87826b04
Brando Miranda <brando90@mit.edu>
2018-06-15 17:43:26 -0400

    before delation of stats of wnorms

--- wnorms_vs_gen_error.m
+++ wnorms_vs_gen_error.m
@@ -4,23 +4,14 @@ dot = '/Users/brandomiranda/home_simulation_research/overparametrized_experiment
 path_all_expts = fullfile(dot,'pytorch_experiments/test_runs_flatness2');
 %% experiment files names
 list_names = [];
-name = "flatness_May_label_corrupt_prob_0.0_exptlabel_MoreRLNLmdls_label_corruption1.0_lr_0.01_momentum_0.9";list_names=[list_names, name];
-name = "flatness_May_label_corrupt_prob_0.0_exptlabel_MoreRLNLmdls_label_corruption0.8_lr_0.01_momentum_0.9";list_names=[list_names, name];
-name = "flatness_May_label_corrupt_prob_0.0_exptlabel_MoreRLNLmdls_label_corruption0.7_lr_0.01_momentum_0.9";list_names=[list_names, name];
-name = "flatness_May_label_corrupt_prob_0.0_exptlabel_MoreRLNLmdls_label_corruption0.6_lr_0.01_momentum_0.9";list_names=[list_names, name];
-name = "flatness_May_label_corrupt_prob_0.0_exptlabel_MoreRLNLmdls_label_corruption0.5_lr_0.01_momentum_0.9";list_names=[list_names, name];
-name = "flatness_May_label_corrupt_prob_0.0_exptlabel_MoreRLNLmdls_label_corruption0.4_lr_0.01_momentum_0.9";list_names=[list_names, name];
-name = "flatness_May_label_corrupt_prob_0.0_exptlabel_MoreRLNLmdls_label_corruption0.3_lr_0.01_momentum_0.9";list_names=[list_names, name];
-name = "flatness_May_label_corrupt_prob_0.0_exptlabel_MoreRLNLmdls_label_corruption0.2_lr_0.01_momentum_0.9";list_names=[list_names, name];
-name = "flatness_May_label_corrupt_prob_0.0_exptlabel_MoreRLNLmdls_label_corruption0.1_lr_0.01_momentum_0.9";list_names=[list_names, name];
-name = "flatness_May_label_corrupt_prob_0.0_exptlabel_MoreRLNLmdls_label_corruption0.05_lr_0.01_momentum_0.9";list_names=[list_names, name];
-name = "flatness_May_label_corrupt_prob_0.0_exptlabel_MoreRLNLmdls_label_corruption0.01_lr_0.01_momentum_0.9";list_names=[list_names, name];
-name = "flatness_May_label_corrupt_prob_0.0_exptlabel_MoreRLNLmdls_label_corruption0.001_lr_0.01_momentum_0.9";list_names=[list_names, name];
-name = "flatness_May_label_corrupt_prob_0.0_exptlabel_MoreRLNLmdls_label_corruption0.0005_lr_0.01_momentum_0.9";list_names=[list_names, name];
-name = "flatness_May_label_corrupt_prob_0.0_exptlabel_MoreRLNLmdls_label_corruption0.0001_lr_0.01_momentum_0.9";list_names=[list_names, name];
-name = "flatness_May_label_corrupt_prob_0.0_exptlabel_MoreRLNLmdls_label_corruption5e-5_lr_0.01_momentum_0.9";list_names=[list_names, name];
-name = "flatness_May_label_corrupt_prob_0.0_exptlabel_MoreRLNLmdls_label_corruption5e-6_lr_0.01_momentum_0.9";list_names=[list_names, name];
-%name = "flatness_May_label_corrupt_prob_0.0_exptlabel_SGD_ManyRuns_Momentum0.9";list_names=[list_names, name];
+name = "flatness_June_label_corrupt_prob_0.0_exptlabel_RLNL_1.0_only_1st_layer_BIAS_False_lr_0.01_momentum_0.9";list_names=[list_names, name];
+name = "flatness_June_label_corrupt_prob_0.0_exptlabel_RLNL_0.75_only_1st_layer_BIAS_False_lr_0.01_momentum_0.9";list_names=[list_names, name];
+name = "flatness_June_label_corrupt_prob_0.0_exptlabel_RLNL_0.5_only_1st_layer_BIAS_False_lr_0.01_momentum_0.9";list_names=[list_names, name];
+name = "flatness_June_label_corrupt_prob_0.0_exptlabel_RLNL_0.1_only_1st_layer_BIAS_False_lr_0.01_momentum_0.9";list_names=[list_names, name];
+name = "flatness_June_label_corrupt_prob_0.0_exptlabel_RLNL_0.0001_only_1st_layer_BIAS_False_lr_0.01_momentum_0.9";list_names=[list_names, name];
+name = "flatness_June_label_corrupt_prob_0.0_exptlabel_RLNL_0.01_only_1st_layer_BIAS_False_lr_0.01_momentum_0.9";list_names=[list_names, name];
+name = "flatness_June_label_corrupt_prob_0.0_exptlabel_RLNL_0.001_only_1st_layer_BIAS_False_lr_0.01_momentum_0.9";list_names=[list_names, name];
+name = "flatness_June_label_corrupt_prob_0.0_exptlabel_NL_only_1st_layer_BIAS_True_lr_0.01_momentum_0.9";list_names=[list_names, name];
 %%
 target_loss = 0.0044
 %% 
@@ -34,13 +25,24 @@ scatter(w_all_norms,gen_errors);
 %plot(w_norms_means,gen_errors_means)
 %scatter(w_norms_means,gen_errors_means)
 lsline
-xlabel('product norm, ||w_k||...||w_1||')
+%%
+norm = 'forbenius_norm';
+xlabel('forbenius norm (of all weights): ||W||_F')
+title('forbenius norm ||W|| vs generalization error')
+%%
+% norm = 'product_norm';
+% xlabel('product norm: ||w_k||...||w_1||')
+% title('product norm ||w_k||...||w_1|| vs generalization error')
+% % %%
+% norm = 'log_product_norm';
+% xlabel('log of product norm: log||w_k||+...+log||w_1||')
+% title('log of product norm log||w_k||+...+log||w_1|| vs generalization error')
+%%
 ylabel('generalization error/test error')
-title('||w|| vs generalization error')
 %errorbar(w_norms_means,gen_errors_means,gen_errors_stds)
 %% save figure
-saveas(fig,'gen_w_norms');
-saveas(fig,'gen_w_norms','pdf');
+saveas(fig,['gen_w_norms' norm]);
+saveas(fig,['gen_w_norms' norm],'pdf');
 %% HELPER FUNCTIONS
 function [w_all_norms, gen_all_errors] = extract_w_norms_vs_gen_errors_results(path_all_expts,list_names,target_loss)
 w_all_norms = [];
@@ -91,8 +93,10 @@ for expt_file_name = expt_data_filenames
     if epoch ~= -1
         train_error = train_errors(epoch);
         if train_error == 0
-            %w_norm = sum(w_norms(:,epoch))
-            w_norm = prod(w_norms(:,epoch))
+            w_norm = sum( w_norms(:,epoch).^2 )^0.5;
+            %w1_norm = (w_norms(1,epoch)^2+w_norms(2,epoch)^2)^0.5;
+            %w_norm = w1_norm*prod(w_norms(3:end,epoch));
+            %w_norm = log(w_norm);
             gen_error = test_errors(epoch)
             gen_errors = [gen_errors gen_error];
             w_norms_all = [w_norms_all w_norm];
