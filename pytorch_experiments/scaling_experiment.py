60a2c22f61ef98121d3e3c119b9a87598ae0c306
Brando Miranda <brando90@mit.edu>
2018-06-17 00:43:21 -0400

    good progress

--- pytorch_experiments/scaling_experiment.py
+++ pytorch_experiments/scaling_experiment.py
@@ -12,6 +12,9 @@ from good_minima_discriminator import divide_params_by
 
 from maps import NamedDict
 
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
 from pdb import set_trace as st
 
 def get_corruption_label( path_to_experiment ):
@@ -29,36 +32,35 @@ class Normalizer:
         :param standardize: x - mu / std , [-1,+1]
         :return:
         '''
-        self.normalization_scheme = normalization_scheme
+        self.normalize = normalization_scheme
         ''' '''
         self.error = metrics.error_criterion
         self.loss = torch.nn.CrossEntropyLoss()
         self.iterations = iterations
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.trainset, self.trainloader, self.testset, self.testloader, self.classes_data = data_class.get_cifer_data_processors(data_path,batch_size_train,batch_size_test,num_workers,label_corrupt_prob,standardize=standardize)
+        ''' dta we are collecting '''
+        self.train_all_losses = []
+        self.gen_all_errors = []
+        ##
+        self.epoch_all_numbers = []
+        self.corruption_all_probs = []
 
     def extract_all_results_vs_test_errors(self,path_all_expts,list_names,target_loss):
-        ## main 2
-        train_all_losses = []
-        gen_all_errors = []
-        ##
-        epoch_all_numbers = []
-        corruption_all_probs = []
-        ##
         for name in list_names:
             path_to_folder_expts = os.path.join(path_all_expts,name)
             results = self.extract_results_with_target_loss(path_to_folder_expts, target_loss)
             ''' extend results ''' #
-            train_all_losses.extend(results.train_losses)
-            gen_all_errors.extend(results.gen_errors)
-        return train_all_losses,gen_all_errors
+            self.train_all_losses.extend(results.train_losses)
+            self.gen_all_errors.extend(results.gen_errors)
+        return self.train_all_losses, self.gen_all_errors
 
     def extract_results_with_target_loss(self,path_to_folder_expts,target_loss):
         train_losses = []
         gen_errors = []
         ## TODO
-        #epoch_numbers = []
-        #orruption_probs = []
+        #epoch_numbers = [] # TODO
+        #orruption_probs = [] # TODO
         ''' '''
         matlab_filenames = [filename for filename in os.listdir(path_to_folder_expts) if '.mat' in filename]
         for matlab_filename in matlab_filenames: # essentially looping through all the nets that were trained
@@ -66,11 +68,14 @@ class Normalizer:
             mat_contents = sio.loadmat(matlab_path)
             ''' '''
             epoch,seed_id = self.match_train_error(target_loss, mat_contents)
-            test_error,train_loss = self.get_results_from_normalized_net(epoch,seed_id, path_to_folder_expts)
-            ''' '''
-            train_losses.append(train_loss)
-            gen_errors.append(test_error)
-        results = NamedDict(train_losses=train_losses,gen_errors=gen_errors,epoch_numbers=epoch_numbers,corruption_probs=corruption_probs)
+            #print(seed_id)
+            if seed_id != -1: # if matched train error actually matched something
+                test_error,train_loss = self.get_results_from_normalized_net(epoch,seed_id, path_to_folder_expts)
+                ''' '''
+                train_losses.append(train_loss)
+                gen_errors.append(test_error)
+        #results = NamedDict(train_losses=train_losses,gen_errors=gen_errors,epoch_numbers=epoch_numbers,corruption_probs=corruption_probs)
+        results = NamedDict(train_losses=train_losses, gen_errors=gen_errors)
         return results
 
     def match_train_error(self,target_loss, mat_contents):
@@ -78,6 +83,7 @@ class Normalizer:
         train_errors = mat_contents['train_errors'][0]
         for epoch in range(len(train_losses)):
             train_loss = train_losses[epoch]
+            #print(train_loss)
             if abs(train_loss - target_loss) < 0.0001:
                 train_error = train_errors[epoch]
                 if train_error == 0.0:
@@ -89,9 +95,9 @@ class Normalizer:
         ''' '''
         ''' get net '''
         nets_folders = [filename for filename in os.listdir(path_to_folder_expts) if 'nets_folder' in filename]
-        net_folder = [filename for filename in nets_folders if f'seed_{seed_id}' in filename] # note seed are unique very h.p.
+        net_folder = [filename for filename in nets_folders if f'seed_{seed_id}' in filename][0] # note seed are unique very h.p.
         net_path = os.path.join(path_to_folder_expts,net_folder)
-        net_name = [net_name for net_name in os.listdir(net_path) if f'epoch_{epoch}' in filename]
+        net_name = [net_name for net_name in os.listdir(net_path) if f'epoch_{epoch}' in net_name][0]
         net_path = os.path.join(net_path, net_name)
         net = torch.load(net_path)
         ''' normalize net '''
@@ -147,23 +153,27 @@ def main():
     path_all_expts = '/cbcl/cbcl01/brando90/home_simulation_research/overparametrized_experiments/pytorch_experiments/test_runs_flatness4'
     ''' expt_paths '''
     list_names = []
-    list_names.append('flatness_June_label_corrupt_prob_0.0_exptlabel_NL_only_1st_layer_BIAS_True_batch_size_train_1024_lr_0.1_momentum_0.9_scheduler_milestones_[200, 250, 300]_gamma_1.0')
-    list_names.append('flatness_June_label_corrupt_prob_0.0_exptlabel_RLNL_0.0001_only_1st_layer_BIAS_True_batch_size_train_256_lr_0.1_momentum_0.9_scheduler_milestones_[200, 250, 300]_gamma_1.0')
-    list_names.append('flatness_June_label_corrupt_prob_0.0_exptlabel_RLNL_0.001_only_1st_layer_BIAS_True_batch_size_train_256_lr_0.1_momentum_0.9_scheduler_milestones_[200, 250, 300]_gamma_1.0')
-    list_names.append('flatness_June_label_corrupt_prob_0.0_exptlabel_RLNL_0.1_only_1st_layer_BIAS_True_batch_size_train_256_lr_0.1_momentum_0.9_scheduler_milestones_[200, 250, 300]_gamma_1.0')
-    list_names.append('flatness_June_label_corrupt_prob_0.0_exptlabel_RLNL_0.5_only_1st_layer_BIAS_True_batch_size_train_256_lr_0.1_momentum_0.9_scheduler_milestones_[200, 250, 300]_gamma_1.0')
-    list_names.append('flatness_June_label_corrupt_prob_0.0_exptlabel_RLNL_0.75_only_1st_layer_BIAS_True_batch_size_train_256_lr_0.1_momentum_0.9_scheduler_milestones_[200, 250, 300]_gamma_1.0')
-    list_names.append('flatness_June_label_corrupt_prob_0.0_exptlabel_RLNL_1.0_only_1st_layer_BIAS_True_batch_size_train_256_lr_0.1_momentum_0.9_scheduler_milestones_[200, 250, 300]_gamma_1.0')
+    list_names.append('flatness_June_label_corrupt_prob_0.0_exptlabel_NL_only_1st_layer_BIAS_True_batch_size_train_1024_lr_0.01_momentum_0.9_scheduler_milestones_[200, 250, 300]_gamma_1.0')
+    list_names.append('flatness_June_label_corrupt_prob_0.0_exptlabel_RLNL_0.0001_only_1st_layer_BIAS_True_batch_size_train_256_lr_0.01_momentum_0.9_scheduler_milestones_[200, 250, 300]_gamma_1.0')
+    list_names.append('flatness_June_label_corrupt_prob_0.0_exptlabel_RLNL_0.001_only_1st_layer_BIAS_True_batch_size_train_256_lr_0.01_momentum_0.9_scheduler_milestones_[200, 250, 300]_gamma_1.0')
+    list_names.append('flatness_June_label_corrupt_prob_0.0_exptlabel_RLNL_0.1_only_1st_layer_BIAS_True_batch_size_train_256_lr_0.01_momentum_0.9_scheduler_milestones_[200, 250, 300]_gamma_1.0')
+    list_names.append('flatness_June_label_corrupt_prob_0.0_exptlabel_RLNL_0.2_only_1st_layer_BIAS_True_batch_size_train_256_lr_0.01_momentum_0.9_scheduler_milestones_[200, 250, 300]_gamma_1.0')
+    list_names.append('flatness_June_label_corrupt_prob_0.0_exptlabel_RLNL_0.5_only_1st_layer_BIAS_True_batch_size_train_256_lr_0.01_momentum_0.9_scheduler_milestones_[200, 250, 300]_gamma_1.0')
+    list_names.append('flatness_June_label_corrupt_prob_0.0_exptlabel_RLNL_0.75_only_1st_layer_BIAS_True_batch_size_train_256_lr_0.01_momentum_0.9_scheduler_milestones_[200, 250, 300]_gamma_1.0')
+    list_names.append('flatness_June_label_corrupt_prob_0.0_exptlabel_RLNL_1.0_only_1st_layer_BIAS_True_batch_size_train_256_lr_0.01_momentum_0.9_scheduler_milestones_[200, 250, 300]_gamma_1.0')
     ''' normalization scheme '''
     normalization_scheme = lambda net: divide_params(net,frobenius_normalization)
-    normalization_scheme = lambda net: divide_params(net, l1_normalization)
-    normalization_scheme = lambda net: divide_params(net, spectral_normalization)
+    #normalization_scheme = lambda net: divide_params(net, l1_normalization)
+    #normalization_scheme = lambda net: divide_params(net, spectral_normalization)
     ''' get results'''
     data_path = './data'
     target_loss = 0.0044
     normalizer = Normalizer(data_path,normalization_scheme)
     train_all_losses, gen_all_errors = normalizer.extract_all_results_vs_test_errors(path_all_expts,list_names,target_loss)
     print(f'train_all_losses,gen_all_errors={train_all_losses,gen_all_errors}')
+    ''' plot '''
+    plt.plot(train_all_losses,gen_all_errors)
+    plt.show()
 
 if __name__ == '__main__':
     main()
